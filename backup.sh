d1d219f7db001d08057bc22cd294786b81712edf
Magnus Jaaska <jaaska@gmail.com>
2021-11-30 11:06:24 +0200

    Kommentaarid

--- backup.sh
+++ backup.sh
@@ -1,15 +1,19 @@
 #/bin/bash
-
+#Määrab kaustade asukohad ja hetkekasutaja.
 kasutaja=$(whoami)
 kodukasutaja_kaust=/home/$kasutaja
-
+#Määrab tänase kuupäeva
 kuupaev=$(date +%d.%m.%Y)
 
+#Annab teada hetkekasutaja ja küsib kasutaja kelle kausta varundada tahame.
 #echo "Hetke kasutaja on $kasutaja"
 echo -n "Sisesta kasutaja keda varundada tahad: (Vaikeväärtus: $kasutaja)"
 read backup_user
+#Määrab tühja väärtuse korral hetkekasutaja muutujale.
 backup_user="${backup_user:=$kasutaja}"
 backitav_folder=/home/$backup_user
+
+#Kontrollib kausta olemasolu ja teeb backupi. Vea korral annab sellest teada.
 
 if [ -d /home/$backup_user ]; then tar cv $backitav_folder | gzip > /home_bcp/$backup_user.$kuupaev.tar.gz
 else
